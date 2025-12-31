be05b309e1ed86e2b8b28331a6ce0040a5cb53c0
Suja216 <chandrasekarans@student.rcdsb.on.ca>
2025-12-31 12:18:01 -0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,8 @@
+numbers = [5,4,3,2,1 ]
+print(numbers)
+print(numbers[4])
+print(numbers[3])
+print(numbers[2])
+print(numbers[1])
+print(numbers[0])
+#numbers[1], numbers[3]= numbers[3], numbers[1]
